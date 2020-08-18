0b47f7d86dedbc0e26fbe9c0eab50e082738f934
Demola Malomo <malomo.alaba@yahoo.com>
2020-08-18 22:40:47 +0100

    palindromes algorithmn function

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,5 @@
 //Reverse a string function
 const reverseString = (str: string): string => {
-
 	//first method
 	/*let strArray = str.split('');
 	let revArray = strArray.reverse();
@@ -14,9 +13,9 @@ const reverseString = (str: string): string => {
 	// return str.split('').reverse().join('');
 
 	//writing from scratch
-	let revWord = ''
+	let revWord = '';
 	for (let i = str.length - 1; i >= 0; i--) {
-		revWord += str[i]
+		revWord += str[i];
 	}
 	return revWord;
 };
@@ -25,10 +24,21 @@ const reverseString = (str: string): string => {
 const factorialize = (num: number): number => {
 	let result = 1;
 	for (let i = 1; i <= num; i++) {
-		result *= i
+		result *= i;
 	}
-	return result
-}
+	return result;
+};
 
-//Palindromes
+//Palindromes: Checks if a word is spelled the same way backward and forward eg eye
+const palindromes = (word: string): boolean => {
+	//replacing special character with empty string
+	let regex = /[\w_]/g;
+	let finalWord = word.toLowerCase().replace(regex, '');
 
+	for (let i = word.length - 1; i >= 0; i--) {
+		finalWord += word[i];
+	}
+
+	return word === finalWord;
+};
+palindromes('eyebb');
